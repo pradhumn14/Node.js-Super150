4be952c8cbe8478856ffec319f9948dbf75d5245
Pradhumn <pradhumnagrawal14@gmail.com>
2023-01-18 11:29:44 +0530

    lecture-3

--- Node/Lec3/HTTP/index.js
+++ Node/Lec3/HTTP/index.js
@@ -3,13 +3,30 @@ const http = require("http")
 
 const server = http.createServer((req, res)=>{
 
+
+    console.log(req);
+
     if(req.url == '/home') {
 
         // res.write("Hello World")
 
         res.write('<h1>Welcome to http server</h1>')
-        
+
         res.end();
+    }
+
+    else if (req.url == '/contact'){
+
+        res.write('Welcome to contact page');
+
+        res.end();
+
+    }
+
+    else {
+
+        res.end('error! you are at wrong page');
+
     }
 
 
@@ -18,3 +35,4 @@ const server = http.createServer((req, res)=>{
 server.listen(3000)
 
 console.log("Server is running on port 3000")
+
